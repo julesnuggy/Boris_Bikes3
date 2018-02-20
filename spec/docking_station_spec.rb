cdce8fcc03d296e224bf3611da81ed7867da0ce1
Christopher Ly <demedial@gmail.com>
2018-02-20 15:40:18 +0000

    Files updated, completed up to chapter 10

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -2,5 +2,9 @@ require 'docking_station'
 
 describe DockingStation do
   it { is_expected.to respond_to :release_bike }
+  it { expect(subject.release_bike).to be_a Bike }
+  it { expect(subject.release_bike).to respond_to :working? }
 end
- 
+
+# a) gets a Bike
+# b) expects the bike to be working
