0abc5758f6bbfb0e841645bde3ea874a5cf5e51d
julesnuggy <julesnuggy@gmail.com>
2018-02-21 15:39:45 +0000

    added error handling

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,24 +1,42 @@
 class DockingStation
+  attr_reader :bike_available
+
+  def initialize
+    @bike_available = true
+  end
 
   def release_bike
-    @bike = Bike.new
+    error = "No bikes at this station"
+
+      if @bike_available == true
+        @bike_available = false
+        @bike = Bike.new
+      else
+        begin
+          raise ArgumentError.new(error)
+        rescue ArgumentError => e
+          puts e.message
+          exit
+        end
+      end
   end
 
   def dock(bike)
     bike.dock
+    @bike_available = true
   end
 
 end
 
 class Bike
-  attr_reader :docked
+  attr_reader :dock_status
 
   def initialize
-    @docked = "undocked"
+    @dock_status = "undocked"
   end
 
   def dock
-    @docked = "docked"
+    @dock_status = "docked"
   end
 
   def working?
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -5,6 +5,14 @@ describe DockingStation do
   it { expect(subject.release_bike).to be_a Bike }
   it { expect(subject.release_bike.working?).to eq true }
   it { is_expected.to respond_to :dock }
+
+  it "raises an errror when no bike available" do
+    expect {
+      subject.release_bike
+      subject.release_bike
+    }.to raise_error
+  end
+
 end
 
 # a) gets a Bike
