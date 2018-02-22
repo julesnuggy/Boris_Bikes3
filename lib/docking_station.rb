a98885afc64be1f707ac50632c780f824da4183e
julesnuggy <julesnuggy@gmail.com>
2018-02-22 15:43:53 +0000

    Added bike selection functionality

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -9,25 +9,58 @@ class DockingStation
     @capacity.times{@bikes_in_station << Bike.new}
   end
 
-  def release_bike
-    fail "No bikes at this station" if @bikes_in_station.empty?
-    fail "Bike is broken" if @bikes_in_station.last.working == false
-      @bikes_in_station.last.release
-      @bikes_in_station.pop
+  def release_bike(idx)
+    fail "No bikes at this station" if @bikes_in_station.count(nil) == DEFAULT_CAPACITY
+    fail "No bike here" if @bikes_in_station[idx] == nil
+    fail "Bike is broken" if @bikes_in_station[idx].working == false
+      released_bike = @bikes_in_station[idx]
+      released_bike.release
+      @bikes_in_station[idx] = nil
+      released_bike
   end
 
-  def dock(bike)
+  def dock(bike, idx)
     fail "The station is full" if full?
-      @bikes_in_station.push(bike)
+    fail "Dock is occupied" if @bikes_in_station[idx] != nil
+      @bikes_in_station[idx] = bike
       bike.dock
+  end
+
+  def view_spaces
+    empty_spaces = []
+    @bikes_in_station.each_with_index { |val, index|
+      empty_spaces.push(index) if val == nil
+    }
+    p "Empty spaces found at #{empty_spaces}"
+  end
+
+  def view_working
+    working_bikes = []
+    @bikes_in_station.each_with_index { |val, index|
+      if val != nil
+        working_bikes.push(index) if val.working
+      end
+    }
+    p "Working bikes found at #{working_bikes}"
+  end
+
+
+  def view_broken
+    broken_bikes = []
+    @bikes_in_station.each_with_index { |val, index|
+      if val != nil
+        broken_bikes.push(index) if !val.working
+      end
+    }
+    p "Broken bikes found at #{broken_bikes}"
   end
 
   private
   def full?
-    if @bikes_in_station.length >= @capacity
-      true
-    else
+    if @bikes_in_station.include?(nil)
       false
+    else
+      true
     end
   end
 
@@ -55,4 +88,12 @@ class Bike
     @working = false
   end
 
+end
+
+
+class Van
+
+end
+
+class Garage
 end
